28f8eef5bade2a561db3ae5be30c3cadaa31b25b
Bruno Barbieri <brunobar79@gmail.com>
2021-07-30 02:05:44 -0400

    clean up

--- demo.ts
+++ demo.ts
@@ -1,12 +1,16 @@
-import { JsonRpcProvider } from '@ethersproject/providers';
-import { suggestFees } from './src';
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { suggestFees } from "./src";
 
-const main = async() => {
-    const provider = new JsonRpcProvider(`https://ropsten.infura.io/v3/${YOUR_API_KEY}`);
-    const ret = await suggestFees(provider);
-    console.log('Result');
-    console.log(ret);
-    console.log('done');
-}
+const YOUR_API_KEY = "";
+
+const main = async () => {
+  const provider = new JsonRpcProvider(
+    `https://ropsten.infura.io/v3/${YOUR_API_KEY}`
+  );
+  const ret = await suggestFees(provider);
+  console.log("Result");
+  console.log(ret);
+  console.log("done");
+};
 
 main();
