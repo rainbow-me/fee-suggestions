60b9d59ff397207463e9a4ce4c3436168f5f12b1
Esteban Miño <efmino@uc.cl>
2021-10-26 12:44:25 -0300

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { JsonRpcProvider } from "@ethersproject/providers";
+import { JsonRpcProvider } from '@ethersproject/providers';
 import { ema } from 'moving-averages';
-import { FeeHistoryResponse, MaxFeeSuggestions, MaxPriorityFeeSuggestions, Suggestions } from "./entities";
-import { toGwei } from "./utils";
+import {
+  FeeHistoryResponse,
+  MaxFeeSuggestions,
+  MaxPriorityFeeSuggestions,
+  Suggestions,
+} from './entities';
+import { toGwei } from './utils';
 
 // samplingCurve is a helper function for the base fee percentile range calculation.
 const samplingCurve = (
@@ -24,7 +29,7 @@ const samplingCurve = (
   );
 };
 
-const  linearRegression = (y: number[],x: number[]) => {
+const linearRegression = (y: number[], x: number[]) => {
   const n = y.length;
   let sum_x = 0;
   let sum_y = 0;
@@ -32,20 +37,20 @@ const  linearRegression = (y: number[],x: number[]) => {
   let sum_xx = 0;
 
   for (let i = 0; i < y.length; i++) {
-      const cY = Number(y[i])
-      const cX = Number(x[i])
-      sum_x += cX;
-      sum_y += cY;
-      sum_xy += (cX*cY);
-      sum_xx += (cX*cX);
-  } 
-  const slope = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+    const cY = Number(y[i]);
+    const cX = Number(x[i]);
+    sum_x += cX;
+    sum_y += cY;
+    sum_xy += cX * cY;
+    sum_xx += cX * cX;
+  }
+  const slope = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
 
   return slope;
-}
+};
 
 const suggestBaseFee = (
-  baseFee:  number[],
+  baseFee: number[],
   order: number[],
   timeFactor: number,
   sampleMin: number,
@@ -60,11 +65,11 @@ const suggestBaseFee = (
   let sumWeight = 0;
   let result = 0;
   let samplingCurveLast = 0;
-  for (let i = 0; i < order.length; i++) {
+  for (let or of order) {
     sumWeight +=
-      pendingWeight * Math.exp((order[i] - baseFee.length + 1) / timeFactor);
+      pendingWeight * Math.exp((or - baseFee.length + 1) / timeFactor);
     const samplingCurveValue = samplingCurve(sumWeight, sampleMin, sampleMax);
-    result += (samplingCurveValue - samplingCurveLast) * baseFee[order[i]];
+    result += (samplingCurveValue - samplingCurveLast) * baseFee[or];
     if (samplingCurveValue >= 1) {
       return result;
     }
@@ -79,11 +84,15 @@ export const suggestMaxBaseFee = async (
   blockCountHistory = 100,
   sampleMin = 0.1,
   sampleMax = 0.3,
-  maxTimeFactor = 15,
+  maxTimeFactor = 15
 ): Promise<MaxFeeSuggestions> => {
   // feeHistory API call without a reward percentile specified is cheap even with a light client backend because it only needs block headers.
   // Therefore we can afford to fetch a hundred blocks of base fee history in order to make meaningful estimates on variable time scales.
-  const feeHistory: FeeHistoryResponse = await provider.send("eth_feeHistory", [blockCountHistory, fromBlock, []]);
+  const feeHistory: FeeHistoryResponse = await provider.send('eth_feeHistory', [
+    blockCountHistory,
+    fromBlock,
+    [],
+  ]);
   const baseFee: number[] = [];
   const order = [];
   for (let i = 0; i < feeHistory.baseFeePerGas.length; i++) {
@@ -91,9 +100,9 @@ export const suggestMaxBaseFee = async (
     order.push(i);
   }
 
-  const blocksArray = Array.from(Array(blockCountHistory+1).keys())
-  const gweiBaseFees = baseFee.map(wei => toGwei(wei))
-  const trend = linearRegression(gweiBaseFees, blocksArray)
+  const blocksArray = Array.from(Array(blockCountHistory + 1).keys());
+  const gweiBaseFees = baseFee.map((wei) => toGwei(wei));
+  const trend = linearRegression(gweiBaseFees, blocksArray);
 
   // If a block is full then the baseFee of the next block is copied. The reason is that in full blocks the minimal tip might not be enough to get included.
   // The last (pending) block is also assumed to end up being full in order to give some upwards bias for urgent suggestions.
@@ -125,7 +134,7 @@ export const suggestMaxBaseFee = async (
     } else {
       bf = maxBaseFee;
     }
-    result[timeFactor] = Math.round(bf)
+    result[timeFactor] = Math.round(bf);
   }
 
   return { baseFeeSuggestion: Math.max(...result), baseFeeTrend: trend };
@@ -133,38 +142,66 @@ export const suggestMaxBaseFee = async (
 
 export const suggestMaxPriorityFee = async (
   provider: JsonRpcProvider,
-  fromBlock = 'latest',
+  fromBlock = 'latest'
 ): Promise<MaxPriorityFeeSuggestions> => {
-  const feeHistory: FeeHistoryResponse = await provider.send("eth_feeHistory", [10, fromBlock, [10, 20, 25, 30, 40, 50]]);
-  const blocksRewards = feeHistory.reward
-  const blocksRewardsPerc10 = blocksRewards.map(reward => Number(reward[0]))
-  const blocksRewardsPerc20 = blocksRewards.map(reward => Number(reward[1]))
-  const blocksRewardsPerc25 = blocksRewards.map(reward => Number(reward[2]))
-  const blocksRewardsPerc30 = blocksRewards.map(reward => Number(reward[3]))
-  const blocksRewardsPerc40 = blocksRewards.map(reward => Number(reward[4]))
-  const blocksRewardsPerc50 = blocksRewards.map(reward => Number(reward[5]))
+  const feeHistory: FeeHistoryResponse = await provider.send('eth_feeHistory', [
+    10,
+    fromBlock,
+    [10, 20, 25, 30, 40, 50],
+  ]);
+  const blocksRewards = feeHistory.reward;
+  const blocksRewardsPerc10 = blocksRewards.map((reward) => Number(reward[0]));
+  const blocksRewardsPerc20 = blocksRewards.map((reward) => Number(reward[1]));
+  const blocksRewardsPerc25 = blocksRewards.map((reward) => Number(reward[2]));
+  const blocksRewardsPerc30 = blocksRewards.map((reward) => Number(reward[3]));
+  const blocksRewardsPerc40 = blocksRewards.map((reward) => Number(reward[4]));
+  const blocksRewardsPerc50 = blocksRewards.map((reward) => Number(reward[5]));
 
-  const emaPerc10: number = Math.round(ema(blocksRewardsPerc10, blocksRewardsPerc10.length).at(-1))
-  const emaPerc20: number = Math.round(ema(blocksRewardsPerc20, blocksRewardsPerc20.length).at(-1))
-  const emaPerc25: number = Math.round(ema(blocksRewardsPerc25, blocksRewardsPerc25.length).at(-1))
-  const emaPerc30: number = Math.round(ema(blocksRewardsPerc30, blocksRewardsPerc30.length).at(-1))
-  const emaPerc40: number = Math.round(ema(blocksRewardsPerc40, blocksRewardsPerc40.length).at(-1))
-  const emaPerc50: number = Math.round(ema(blocksRewardsPerc50, blocksRewardsPerc50.length).at(-1))
+  const emaPerc10: number = Math.round(
+    ema(blocksRewardsPerc10, blocksRewardsPerc10.length).at(-1)
+  );
+  const emaPerc20: number = Math.round(
+    ema(blocksRewardsPerc20, blocksRewardsPerc20.length).at(-1)
+  );
+  const emaPerc25: number = Math.round(
+    ema(blocksRewardsPerc25, blocksRewardsPerc25.length).at(-1)
+  );
+  const emaPerc30: number = Math.round(
+    ema(blocksRewardsPerc30, blocksRewardsPerc30.length).at(-1)
+  );
+  const emaPerc40: number = Math.round(
+    ema(blocksRewardsPerc40, blocksRewardsPerc40.length).at(-1)
+  );
+  const emaPerc50: number = Math.round(
+    ema(blocksRewardsPerc50, blocksRewardsPerc50.length).at(-1)
+  );
 
-  return { 
-    maxPriorityFeeSuggestions: { urgent: emaPerc40, fast: emaPerc30, normal: emaPerc20 },
+  return {
     confirmationTimeByPriorityFee: {
-        15: emaPerc50,
-        30: emaPerc40,
-        45: emaPerc30,
-        60: emaPerc25,
-        75: emaPerc10,
-      }
-  }
-}
+      15: emaPerc50,
+      30: emaPerc40,
+      45: emaPerc30,
+      60: emaPerc25,
+      75: emaPerc10,
+    },
+    maxPriorityFeeSuggestions: {
+      fast: emaPerc30,
+      normal: emaPerc20,
+      urgent: emaPerc40,
+    },
+  };
+};
 
-export const suggestFees = async (provider: JsonRpcProvider): Promise<Suggestions> => {
-  const { baseFeeSuggestion, baseFeeTrend } = await suggestMaxBaseFee(provider)
-  const { maxPriorityFeeSuggestions, confirmationTimeByPriorityFee } = await suggestMaxPriorityFee(provider)
-  return { maxPriorityFeeSuggestions, baseFeeSuggestion, baseFeeTrend, confirmationTimeByPriorityFee }
-}
+export const suggestFees = async (
+  provider: JsonRpcProvider
+): Promise<Suggestions> => {
+  const { baseFeeSuggestion, baseFeeTrend } = await suggestMaxBaseFee(provider);
+  const { maxPriorityFeeSuggestions, confirmationTimeByPriorityFee } =
+    await suggestMaxPriorityFee(provider);
+  return {
+    baseFeeSuggestion,
+    baseFeeTrend,
+    confirmationTimeByPriorityFee,
+    maxPriorityFeeSuggestions,
+  };
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 import BigNumber from 'bignumber.js';
 
-export const toGwei = (wei:number) => {
-    return new BigNumber(wei).dividedBy(1000000000).toNumber()
-}
-  
+export const toGwei = (wei: number) => {
+  return new BigNumber(wei).dividedBy(1000000000).toNumber();
+};
