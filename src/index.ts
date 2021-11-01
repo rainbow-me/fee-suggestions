5fc7ceea5ab6dd7f6f9e8412445e5cddb387a4d0
Esteban Miño <efmino@uc.cl>
2021-11-01 12:30:06 -0400

    Handle outliers priority fee data values (#12)
    * filteroutliers
    
    * move utils
    
    * change percentiles
    
    * max values
    
    * rename package

--- src/index.ts
+++ src/index.ts
@@ -6,77 +6,13 @@ import {
   MaxPriorityFeeSuggestions,
   Suggestions,
 } from './entities';
-import { gweiToWei, weiToGweiNumber } from './utils';
-
-// samplingCurve is a helper function for the base fee percentile range calculation.
-const samplingCurve = (
-  sumWeight: number,
-  sampleMin: number,
-  sampleMax: number
-) => {
-  if (sumWeight <= sampleMin) {
-    return 0;
-  }
-  if (sumWeight >= sampleMax) {
-    return 1;
-  }
-  return (
-    (1 -
-      Math.cos(
-        ((sumWeight - sampleMin) * 2 * Math.PI) / (sampleMax - sampleMin)
-      )) /
-    2
-  );
-};
-
-const linearRegression = (y: number[], x: number[]) => {
-  const n = y.length;
-  let sumX = 0;
-  let sumY = 0;
-  let sumXY = 0;
-  let sumXX = 0;
-
-  for (let i = 0; i < y.length; i++) {
-    const cY = Number(y[i]);
-    const cX = Number(x[i]);
-    sumX += cX;
-    sumY += cY;
-    sumXY += cX * cY;
-    sumXX += cX * cX;
-  }
-  const slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
-
-  return slope;
-};
-
-const suggestBaseFee = (
-  baseFee: number[],
-  order: number[],
-  timeFactor: number,
-  sampleMin: number,
-  sampleMax: number
-) => {
-  if (timeFactor < 1e-6) {
-    return baseFee[baseFee.length - 1];
-  }
-  const pendingWeight =
-    (1 - Math.exp(-1 / timeFactor)) /
-    (1 - Math.exp(-baseFee.length / timeFactor));
-  let sumWeight = 0;
-  let result = 0;
-  let samplingCurveLast = 0;
-  for (let or of order) {
-    sumWeight +=
-      pendingWeight * Math.exp((or - baseFee.length + 1) / timeFactor);
-    const samplingCurveValue = samplingCurve(sumWeight, sampleMin, sampleMax);
-    result += (samplingCurveValue - samplingCurveLast) * baseFee[or];
-    if (samplingCurveValue >= 1) {
-      return result;
-    }
-    samplingCurveLast = samplingCurveValue;
-  }
-  return result;
-};
+import {
+  gweiToWei,
+  linearRegression,
+  rewardsFilterOutliers,
+  suggestBaseFee,
+  weiToGweiNumber,
+} from './utils';
 
 export const suggestMaxBaseFee = async (
   provider: JsonRpcProvider,
@@ -86,8 +22,6 @@ export const suggestMaxBaseFee = async (
   sampleMax = 0.3,
   maxTimeFactor = 15
 ): Promise<MaxFeeSuggestions> => {
-  // feeHistory API call without a reward percentile specified is cheap even with a light client backend because it only needs block headers.
-  // Therefore we can afford to fetch a hundred blocks of base fee history in order to make meaningful estimates on variable time scales.
   const feeHistory: FeeHistoryResponse = await provider.send('eth_feeHistory', [
     blockCountHistory,
     fromBlock,
@@ -103,8 +37,6 @@ export const suggestMaxBaseFee = async (
   const blocksArray = Array.from(Array(blockCountHistory + 1).keys());
   const trend = linearRegression(baseFees, blocksArray);
 
-  // If a block is full then the baseFee of the next block is copied. The reason is that in full blocks the minimal tip might not be enough to get included.
-  // The last (pending) block is also assumed to end up being full in order to give some upwards bias for urgent suggestions.
   baseFees[baseFees.length - 1] *= 9 / 8;
   for (let i = feeHistory.gasUsedRatio.length - 1; i >= 0; i--) {
     if (feeHistory.gasUsedRatio[i] > 0.9) {
@@ -150,60 +82,41 @@ export const suggestMaxPriorityFee = async (
   const feeHistory: FeeHistoryResponse = await provider.send('eth_feeHistory', [
     10,
     fromBlock,
-    [10, 20, 25, 30, 40, 50],
+    [10, 15, 30, 45],
   ]);
   const blocksRewards = feeHistory.reward;
 
   if (!blocksRewards.length) throw new Error('Error: block reward was empty');
 
-  const blocksRewardsPerc10 = blocksRewards.map((reward) =>
-    weiToGweiNumber(reward[0])
-  );
-  const blocksRewardsPerc20 = blocksRewards.map((reward) =>
-    weiToGweiNumber(reward[1])
-  );
-  const blocksRewardsPerc25 = blocksRewards.map((reward) =>
-    weiToGweiNumber(reward[2])
-  );
-  const blocksRewardsPerc30 = blocksRewards.map((reward) =>
-    weiToGweiNumber(reward[3])
-  );
-  const blocksRewardsPerc40 = blocksRewards.map((reward) =>
-    weiToGweiNumber(reward[4])
-  );
-  const blocksRewardsPerc50 = blocksRewards.map((reward) =>
-    weiToGweiNumber(reward[5])
-  );
+  const blocksRewardsPerc10 = rewardsFilterOutliers(blocksRewards, 0);
+  const blocksRewardsPerc15 = rewardsFilterOutliers(blocksRewards, 1);
+  const blocksRewardsPerc30 = rewardsFilterOutliers(blocksRewards, 2);
+  const blocksRewardsPerc45 = rewardsFilterOutliers(blocksRewards, 3);
 
   const emaPerc10 = ema(blocksRewardsPerc10, blocksRewardsPerc10.length).at(-1);
-  const emaPerc20 = ema(blocksRewardsPerc20, blocksRewardsPerc20.length).at(-1);
-  const emaPerc25 = ema(blocksRewardsPerc25, blocksRewardsPerc25.length).at(-1);
+  const emaPerc15 = ema(blocksRewardsPerc15, blocksRewardsPerc15.length).at(-1);
   const emaPerc30 = ema(blocksRewardsPerc30, blocksRewardsPerc30.length).at(-1);
-  const emaPerc40 = ema(blocksRewardsPerc40, blocksRewardsPerc40.length).at(-1);
-  const emaPerc50 = ema(blocksRewardsPerc50, blocksRewardsPerc50.length).at(-1);
+  const emaPerc45 = ema(blocksRewardsPerc45, blocksRewardsPerc45.length).at(-1);
 
   if (
     emaPerc10 === undefined ||
-    emaPerc20 === undefined ||
-    emaPerc25 === undefined ||
+    emaPerc15 === undefined ||
     emaPerc30 === undefined ||
-    emaPerc40 === undefined ||
-    emaPerc50 === undefined
+    emaPerc45 === undefined
   )
-    throw new Error('Error: ema was empty');
+    throw new Error('Error: ema was undefined');
 
   return {
     confirmationTimeByPriorityFee: {
-      15: gweiToWei(emaPerc50),
-      30: gweiToWei(emaPerc40),
-      45: gweiToWei(emaPerc30),
-      60: gweiToWei(emaPerc25),
-      75: gweiToWei(emaPerc10),
+      15: gweiToWei(emaPerc45),
+      30: gweiToWei(emaPerc30),
+      45: gweiToWei(emaPerc15),
+      60: gweiToWei(emaPerc10),
     },
     maxPriorityFeeSuggestions: {
-      fast: gweiToWei(emaPerc30),
-      normal: gweiToWei(emaPerc20),
-      urgent: gweiToWei(emaPerc40),
+      fast: gweiToWei(Math.max(emaPerc30, 1.5)),
+      normal: gweiToWei(Math.max(emaPerc15, 1)),
+      urgent: gweiToWei(Math.max(emaPerc45, 2)),
     },
   };
 };
